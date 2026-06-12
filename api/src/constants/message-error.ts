c3b23262908f85e2c64fcb648b2a246eb0765fd6
Kiet.DuongMinh <toilakiet24@gmail.com>
2026-06-12 10:21:44 +0700

    feat(Error) : message error

---
+++ api/src/constants/message-error.ts
@@ -0,0 +1,8 @@
+export const ERROR = {
+  CFLEMAIL: 'Email is exist.',
+  EVLLOGIN: 'Invalid email or password.',
+  SVLOGIN: 'Server is not available to login.',
+  SVLOGOUT: 'Server is not available to logout.',
+  SVREGISTER: 'Server is not available to register.',
+  SVENV: 'Can not get environment variable.',
+};
